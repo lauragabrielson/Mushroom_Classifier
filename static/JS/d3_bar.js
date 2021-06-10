2c7301adf65200fb2c56894b38c9ee190d2e9ace
beaujeffrey03 <75648641+beaujeffrey03@users.noreply.github.com>
2021-06-10 18:24:55 -0500

    Got the attribute button group set up and update d3 bar chart working.

--- static/JS/d3_bar.js
+++ static/JS/d3_bar.js
@@ -1,6 +1,9 @@
 console.log('d3_bar.js loaded')
 
-dataFile = "../static/data/bar_chart_csv/cap_surface.csv"
+dataClass = "../static/data/bar_chart_csv/class.csv"
+dataCapColor = "../static/data/bar_chart_csv/cap_color.csv"
+dataCapShape = "../static/data/bar_chart_csv/cap_shape.csv"
+dataCapSurface = "../static/data/bar_chart_csv/cap_surface.csv"
 
 var svgWidth = 800;
 var svgHeight = 500;
@@ -22,17 +25,15 @@ var svg = d3.select("#explore-bar")
   .append("svg")
   .attr("height", svgHeight)
   .attr("width", svgWidth)
-  .style("background-color", "#f2f2f2")
-  .style("margin", "20px");
+  .style("margin", "20px")
+  .attr("class", "visual");
 
 // Append a group to the SVG area, translate to right and bottom
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
 
 // Load data
-d3.csv(dataFile).then(function(mushData) {
-
-  console.log(mushData);
+d3.csv(dataClass).then(function(mushData) {
 
   // Cast the count value to a number for each piece of data
   mushData.forEach(function(d) {
@@ -82,7 +83,7 @@ d3.csv(dataFile).then(function(mushData) {
     .ease(d3.easeCubicInOut)
     .attr("y", d => yLinearScale(d.count))
     .attr("height", d => chartHeight - yLinearScale(d.count))
-    .delay(function(d,i){console.log(i) ; return(i*100)});
+    .delay(function(d,i){return(i*100)});
 
   chartGroup.selectAll("text")
     .data(mushData)
@@ -99,3 +100,77 @@ d3.csv(dataFile).then(function(mushData) {
 }).catch(function(error) {
   console.log(error);
 });
+
+
+function updateBar(ClickedData) {
+  // Load data
+d3.csv(ClickedData).then(function(mushData) {
+
+  // Cast the count value to a number for each piece of data
+  mushData.forEach(function(d) {
+    d.count = +d.count;
+  });
+
+  // Configure a band scale for the horizontal axis with a padding of 0.1 (10%)
+  var xBandScale = d3.scaleBand()
+    .domain(mushData.map(d => d.value))
+    .range([0, chartWidth])
+    .padding(0.1);
+
+  // Create a linear scale for the vertical axis.
+  var yLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(mushData, d => d.count)])
+    .range([chartHeight, 0]);
+
+  // Create two new functions passing our scales in as arguments
+  // These will be used to create the chart's axes
+  var bottomAxis = d3.axisBottom(xBandScale);
+  var leftAxis = d3.axisLeft(yLinearScale).ticks(10);
+
+  chartGroup.exit().remove();
+
+  // Append two SVG group elements to the chartGroup area,
+  // and create the bottom and left axes inside of them
+  chartGroup.append("g")
+    .call(leftAxis);
+
+  chartGroup.append("g")
+    .attr("transform", `translate(0, ${chartHeight})`)
+    .call(bottomAxis);
+
+  // Create one SVG rectangle per piece of mushData
+  // Use the linear and band scales to position each rectangle within the chart
+  chartGroup.selectAll(".bar")
+    .data(mushData)
+    .enter()
+    .append("rect")
+    .style("fill", d => d.color)
+    .attr("x", d => xBandScale(d.value))
+    .attr("y", chartHeight)
+    .attr("width", xBandScale.bandwidth())
+    .attr("height", d => chartHeight - yLinearScale(0))
+
+  chartGroup.selectAll("rect")
+    .transition()
+    .duration(700)
+    .ease(d3.easeCubicInOut)
+    .attr("y", d => yLinearScale(d.count))
+    .attr("height", d => chartHeight - yLinearScale(d.count))
+    .delay(function(d,i){return(i*100)});
+
+  chartGroup.selectAll("text")
+    .data(mushData)
+    .enter()
+    .append("text")
+    .text(d => {console.log(d) ; d.count})
+    .attr("text-anchor", "middle")
+    .attr("x", d => xBandScale(d.value) + xBandScale.bandwidth() / 2)
+    .attr("y", d => chartHeight - yLinearScale(d.count) + 15)
+    .attr("font-family", "sans-serif")
+    .attr("font-size", "11px")
+    .attr("fill", "#black");
+
+}).catch(function(error) {
+  console.log(error);
+});
+}
